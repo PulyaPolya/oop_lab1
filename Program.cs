871751c4e1e527b151b9a120615fddf4649b83da
PulyaPolya <79004071+PulyaPolya@users.noreply.github.com>
2021-04-07 17:17:07 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,54 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+
+    static void Main(string[] args)
+    {
+        Square_Matrix m = new Square_Matrix();
+       
+        string a;
+        int b;
+        m.Set_size(100);
+        /*for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                a = Console.ReadLine();
+                 b = Convert.ToInt32(a);
+                m.Set_element(i, j, b);
+            }
+        }
+
+       for(int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                System.Console.Write(m.GetElement(i,j)+" ");
+            }
+            Console.WriteLine();
+        }*/
+       
+       // Console.WriteLine(m.Get_String());
+        //a = Console.ReadLine();
+    }
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
