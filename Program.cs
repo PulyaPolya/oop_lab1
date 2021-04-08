7e0ad2f88096998210e5627069e0c4dcb850c0d2
PulyaPolya <79004071+PulyaPolya@users.noreply.github.com>
2021-04-08 09:52:34 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,30 +11,12 @@ class Program
     {
         Square_Matrix m = new Square_Matrix();
        
-        string a;
-        int b;
-        m.Set_size(100);
-        /*for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                a = Console.ReadLine();
-                 b = Convert.ToInt32(a);
-                m.Set_element(i, j, b);
-            }
-        }
-
-       for(int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                System.Console.Write(m.GetElement(i,j)+" ");
-            }
-            Console.WriteLine();
-        }*/
-       
-       // Console.WriteLine(m.Get_String());
-        //a = Console.ReadLine();
+      int s;
+        s = Convert.ToInt32(Console.ReadLine());
+        Square_Matrix m = new Square_Matrix(s);
+        m.SetOneValue(1);
+        Console.WriteLine(m.ToString());
+        string a = Console.ReadLine();
     }
    
 }
